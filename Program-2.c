e878e0a64ab1b9b6c81d1d99b8242507457295ec
manikantabp <20me0130@vvce.ac.in>
2021-03-29 10:50:55 +0000

    P2

--- Program-2.c
+++ Program-2.c
@@ -9,8 +9,8 @@ void main()
 	int i=1,n;
 	printf("Enter upper limit : ");
 	scanf("%d",&n);
-	printf ("The natural numbers are ");
+	printf("The natural numbers are ");
 	for(i=1;i<=n;i++)
-  printf("%d ",i);
- //Fill the missing code here
+		printf("%d ",i);
+	//Fill the missing code here
  }
